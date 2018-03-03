a5bd81e4169f4360f9a6e6f3f267c0d7e1114ab5
Artur <arko8919@gmail.com>
2018-03-03 21:10:17 +0000

    Project Finished

--- js/main.js
+++ js/main.js
@@ -1,3 +1,59 @@
 "use strict";
 
-$(document).ready(function () {});
+$(document).ready(function () {
+
+    var apiKey = "AIzaSyC9WXiY77CaOj3XDr1H4skAIhnM24vIEIA"; // My Google API KEY
+    var url = "https://www.googleapis.com/urlshortener/v1/url"; //
+
+    $("#expand").click(function () {
+        $("#response").empty(); // Clean Field
+        expand();
+        return false;
+    });
+
+    $("#shorten").click(function () {
+        $("#response").empty(); // Clean Field
+        shorten();
+        return false;
+    });
+
+    $(".copyButton").click(function () {
+        copyToClipboard(".createdElement");
+        return false;
+    });
+
+    function expand() {
+        var urlToExpand = url + "?key=" + apiKey + "&shortUrl=" + $("#input").val();
+
+        $.getJSON(urlToExpand, function (response) {
+            $("#response").append("<span class=\"createdElement\">" + response.longUrl + "</span>");
+        });
+    }
+
+    function shorten() {
+        var urlWithKey = url + "?key=" + apiKey;
+        var urlToShorten = $("#input").val();
+
+        $.post({
+            url: urlWithKey,
+            data: JSON.stringify({ longUrl: urlToShorten }),
+            dataType: "json",
+            contentType: "application/json",
+
+            success: function success(response) {
+                $("#response").append("<span class=\"createdElement\">" + response.id + "</span>");
+            },
+            error: function error(jqXHR, status, errorThrown) {
+                console.log(jqXHR);
+            }
+        });
+    }
+
+    function copyToClipboard(element) {
+        var $temp = $("<input>");
+        $("body").append($temp);
+        $temp.val($(element).text()).select();
+        document.execCommand("copy");
+        $temp.remove();
+    }
+});
--- src/main.js
+++ src/main.js
@@ -1,4 +1,58 @@
-$(document).ready(()=> {
+$(document).ready(() => {
 
+    const apiKey = "AIzaSyC9WXiY77CaOj3XDr1H4skAIhnM24vIEIA"; // My Google API KEY
+    const url = "https://www.googleapis.com/urlshortener/v1/url"; //
 
+    $("#expand").click(() => {
+        $("#response").empty(); // Clean Field
+        expand();
+        return false;
+    });
+
+    $("#shorten").click(() => {
+        $("#response").empty(); // Clean Field
+        shorten();
+        return false;
+    });
+
+    $(".copyButton").click(() => {
+        copyToClipboard(".createdElement");
+        return false;
+    });
+
+    function expand() {
+        const urlToExpand = url + "?key=" + apiKey + "&shortUrl=" + $("#input").val();
+
+        $.getJSON(urlToExpand, response => {
+            $("#response").append(`<span class="createdElement">${response.longUrl}</span>`);
+        });
+    }
+
+    function shorten() {
+        const urlWithKey = url + "?key=" + apiKey;
+        const urlToShorten = $("#input").val();
+
+        $.post({
+            url: urlWithKey,
+            data: JSON.stringify({longUrl: urlToShorten}),
+            dataType: "json",
+            contentType: "application/json",
+
+            success(response) {
+                $("#response").append(`<span class="createdElement">${response.id}</span>`);
+            },
+
+            error(jqXHR, status, errorThrown) {
+                console.log(jqXHR);
+            }
+        })
+    }
+
+    function copyToClipboard(element) {
+        const $temp = $("<input>");
+        $("body").append($temp);
+        $temp.val($(element).text()).select();
+        document.execCommand("copy");
+        $temp.remove();
+    }
 });
